981208ba68f2bf615a27e93bc52924d1b83e425e
Jakub <galy@stud.uniza.sk>
2022-12-11 22:16:10 +0100

    aaa

---
+++ vtaky/zako.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class zako here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class zako {
+    /**
+     * Constructor for objects of class zako
+     */
+    public zako() {
+        System.out.println("dsdssdsdgsd");
+        System.out.println("dsdssdsdgsd");
+        System.out.println("dsdssdsdgsd");
+        System.out.println("dsdssdsdgsd");
+        System.out.println("dsdssdsdgsd");
+        System.out.println("dsdssdsdgsd");
+    }
+}
