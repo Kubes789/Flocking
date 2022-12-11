7f894b534d4ab09a30a00f5803c4be5d79f27b91
Jakub <galy@stud.uniza.sk>
2022-12-11 21:46:40 +0100

    vytvorenie triedy Vtak

---
+++ vtaky/Bird.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Bird here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bird {
+    /**
+     * Constructor for objects of class Bird
+     */
+    public Bird() {
+        // initialise instance variables
+    }
+}
