4c093768a3d3896a439f155ddcb866adb1a1cfdf
Jakub <galy@stud.uniza.sk>
2022-12-11 22:30:18 +0100

    ahojda

---
+++ vtaky/Dodo.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Dodo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dodo {
+    /**
+     * Constructor for objects of class Dodo
+     */
+    public Dodo() {
+        // initialise instance variables
+    }
+}
